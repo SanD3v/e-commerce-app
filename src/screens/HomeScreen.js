5e3241d9ad2af0de045b339254e8f4531d9eaca4
sand3v <santoshbdevadiga143@gmail.com>
2022-09-19 17:31:03 +0530

    image

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,9 @@
-import { FlatList, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
 import React, { Component, useState } from 'react'
-const fetchAPI = ()=>{
+
+const HomeScreen = () => {
+  const [res,resSet] = useState([{brand:'',image:''}])
+  const fetchAPI = ()=>{
     fetch("http://10.0.2.2:3000/api/v1/products",
       {
         'method': 'GET'
@@ -8,29 +11,39 @@ const fetchAPI = ()=>{
     )
     .then(Response=>Response.json())
     .then(Response =>{
-        console.log(Response);
+        resSet(Response)
+        console.log(res);
+        console.log(res[0].image)
+        
     })
     .catch(err=>{
         console.log('errreeo');
         console.log(err);
     });
     };
-export class HomeScreen extends Component {
-  render() {
-    return (
-      <View style={{flex: 1,backgroundColor: '#FFB140'}}>
+  return (
+    <View style={{flex: 1,backgroundColor: '#FFB140'}}>
         <TouchableHighlight 
           onPress={fetchAPI} 
           underlayColor='#FFB140'
           style={style.touchable}
           >
         <View>
-            <Text style={{fontSize:50}}>Call API</Text>
+          <Text style={{fontSize:50}}>Call API</Text>
         </View>
         </TouchableHighlight>
-      </View>
-    )
-  }
+        <Text>{res[0].brand}</Text>
+        <Image 
+          source={{uri: res[0].image.replace('localhost','10.0.2.2')}}
+          style={{width: 400, height: 400}} />
+        <Text>something</Text>
+        {/* <FlatList
+          data={res}
+          renderItem={({ res }) => <Text >{res}</Text>}
+          keyExtractor={(res) => res.id}/> */}
+    </View>
+    
+  )
 }
 const style = StyleSheet.create({
   touchable: {
