45d95e94578dfa96ac41d2e8e07fefcb75af4f48
sand3v <santoshbdevadiga143@gmail.com>
2022-09-22 12:06:32 +0530

    working image

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,49 +1,58 @@
-import { FlatList, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
-import React, { Component, useState } from 'react'
+import { ActivityIndicator, FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+import React, { Component, useEffect, useState } from 'react'
 
 const HomeScreen = () => {
-  const [res,resSet] = useState([{brand:'',image:''}])
-  const fetchAPI = ()=>{
-    fetch("http://10.0.2.2:3000/api/v1/products",
-      {
-        'method': 'GET'
+  const [product,productSet] = useState('');
+  const [isLoading, isLoadingSet] = useState(true);
+  useEffect(()=>{
+    async function fetchProduct(){
+      fetch("http://10.0.2.2:3000/api/v1/products",
+            {
+              'method': 'GET'
+            }
+      )
+      .then(Response=>Response.json())
+      .then(Response =>{
+          productSet(Response);
+          isLoadingSet(false);
+          
+      })
+      .catch(err=>{
+          console.log(err);
+        });
       }
+      fetchProduct();
+    },[]);
+  if(isLoading){
+    return (
+      <ActivityIndicator
+        size={'large'}
+        animating={isLoading}
+      />
     )
-    .then(Response=>Response.json())
-    .then(Response =>{
-        resSet(Response)
-        console.log(res);
-        console.log(res[0].image)
-        
-    })
-    .catch(err=>{
-        console.log('errreeo');
-        console.log(err);
-    });
-    };
-  return (
-    <View style={{flex: 1,backgroundColor: '#FFB140'}}>
-        <TouchableHighlight 
-          onPress={fetchAPI} 
-          underlayColor='#FFB140'
-          style={style.touchable}
-          >
-        <View>
-          <Text style={{fontSize:50}}>Call API</Text>
-        </View>
-        </TouchableHighlight>
-        <Text>{res[0].brand}</Text>
-        <Image 
-          source={{uri: res[0].image.replace('localhost','10.0.2.2')}}
-          style={{width: 400, height: 400}} />
-        <Text>something</Text>
-        {/* <FlatList
-          data={res}
-          renderItem={({ res }) => <Text >{res}</Text>}
-          keyExtractor={(res) => res.id}/> */}
-    </View>
-    
-  )
+  }else{
+    return(
+      <FlatList
+        data={product}
+        renderItem={({ item})=>
+          <View>
+            <Text>{item.brand}</Text>
+            <Image 
+              source={{uri: item.image.replace('localhost','10.0.2.2')}}
+              style={{width: 400, height: 600}} 
+              resizeMode='cover'
+              />
+          </View>
+          }
+        />
+      // <View>
+      //   <Text>{product[0].brand}</Text>
+      //   <Image source={{ uri: product[0].image.replace('localhost','10.0.2.2')}}/>
+      //   <Text>{product[1].brand}</Text>
+      //   <Image source={{ uri: product[1].image.replace('localhost','10.0.2.2')}}/>
+      // </View>
+    )
+  }
 }
 const style = StyleSheet.create({
   touchable: {
