132099b9a6997e0b6ab0452f32c7aac2888a9d24
sand3v <santoshbdevadiga143@gmail.com>
2022-09-25 19:33:34 +0530

    Added Toast messages

--- App.js
+++ App.js
@@ -1,30 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
+import Ionicons from '@expo/vector-icons/Ionicons'
 import HomeScreen from './src/screens/HomeScreen';
 import Details from './src/screens/Details';
+import Cart from './src/screens/Cart'
+import Header from './src/screens/header'
+import Toast from 'react-native-toast-message';
 
 export default function App() {
-  const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer styles={styles.container}>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen 
+      <Header/>
+      <Tab.Navigator 
+        initialRouteName='Home'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'HomeScreen') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Cart') {
+              iconName = focused ? 'cart' : 'cart-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+        >
+        <Tab.Screen 
           name='HomeScreen' 
           component={HomeScreen} 
-          options={
-            {
-              headerTitle: 'Home',
-              headerTitleAlign: 'center',
-              headerStyle: 
-                {
-                  backgroundColor: '#FFB140'
-                }
-            }
-          }/>
-        <Stack.Screen name='Details' component={Details}/>
-      </Stack.Navigator>
+          />
+        <Tab.Screen name='Cart' component={Cart}/>
+      </Tab.Navigator>
+    <Toast/>
     </NavigationContainer>
     
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,7 @@
-import { ActivityIndicator, FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, ScrollView, Button} from 'react-native'
 import React, { Component, useEffect, useState } from 'react'
+import { Left, Right, Container, Heading, NativeBaseProvider, HStack } from 'native-base';
+import Toast from 'react-native-toast-message';
 
 const HomeScreen = () => {
   const [product,productSet] = useState('');
@@ -35,30 +37,124 @@ const HomeScreen = () => {
       <FlatList
         data={product}
         renderItem={({ item})=>
-          <View>
-            <Text>{item.brand}</Text>
-            <Image 
-              source={{uri: item.image.replace('localhost','10.0.2.2')}}
-              style={{width: 400, height: 600}} 
-              resizeMode='cover'
-              />
-          </View>
+          <NativeBaseProvider>
+            {/* <Container style={styles.container}> */}
+            <ScrollView style={{ marginBottom: 10, padding: 5 }}>
+              <View>
+                        <Image 
+                            source={{
+                                uri: item.image.replace('localhost','10.0.2.2')
+                              }}
+                            resizeMode="contain"
+                            style={styles.image}
+                        />
+                    </View>
+                    <View style={styles.contentContainer}>
+                        <Heading style={styles.contentHeader}>{
+                        item.name.length > 25 ? item.name.substring(0, 25 - 3)
+                    + '...' : item.name}</Heading>
+                        <Text style={styles.contentText}>{item.brand}</Text>
+                    </View>
+                    <View style={styles.availabilityContainer}>
+                        <View style={styles.availability}>
+                            <Text style={{ marginRight: 10 }}>
+                                Available
+                            </Text>
+                            <View style={
+                                {
+                                  backgroundColor: '#afec1a',
+                                  borderRadius: 50,
+                                  width: 10,
+                                  height: 10,
+                                  padding: 10,
+                                }
+                              }></View>
+                        </View>
+                        <Text>{item.description}</Text>
+                    </View>
+            </ScrollView>
+            <View style={styles.bottomContainer}>
+                <HStack>
+                  <Text style={styles.price}>$ {item.price}</Text>
+                  <TouchableOpacity 
+                    style={styles.touchable}
+                    onPress={()=>{
+                      Toast.show({
+                        type: 'success',
+                        text1: 'Hello',
+                        text2: 'This is some something 👋'
+                      });
+                    }}
+                    >
+                    <Text style={{ color: 'white'}}>Add</Text>
+                  </TouchableOpacity>
+                </HStack>
+            </View>  
+            {/* </Container>       */}
+          </NativeBaseProvider>
           }
         />
-      // <View>
-      //   <Text>{product[0].brand}</Text>
-      //   <Image source={{ uri: product[0].image.replace('localhost','10.0.2.2')}}/>
-      //   <Text>{product[1].brand}</Text>
-      //   <Image source={{ uri: product[1].image.replace('localhost','10.0.2.2')}}/>
-      // </View>
     )
   }
 }
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   touchable: {
-    backgroundColor: '#F7F06D',
-    alignItems: 'center',
+    flexDirection: 'row',
+    borderRadius: 3,
+    padding: 10,
+    margin: 5,
     justifyContent: 'center',
+    backgroundColor: '#5cb85c',
+    width: 100,
+    alignItems: 'center'
+  },
+  container: {
+    position: 'relative',
+    height: '100%'
+  },
+  imageContainer: {
+      backgroundColor: 'white',
+      padding: 0,
+      margin: 0
+  },
+  image: {
+      width: '100%',
+      height: 250
+  },
+  contentContainer: {
+      marginTop: 20,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  contentHeader: {
+      fontWeight: 'bold',
+      marginBottom: 20
+  },
+  contentText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 20
+  },
+  bottomContainer: {
+    alignItems: 'center',
+      // flexDirection: 'row',
+      // position: 'absolute',
+      // bottom: 0,
+      // left: 0,
+      // backgroundColor: 'white'
+  },
+  price: {
+      fontSize: 24,
+      margin: 20,
+      color: 'red'
+  },
+  availabilityContainer: {
+      marginBottom: 20,
+      alignItems: "center"
+  },
+  availability: {
+      flexDirection: 'row',
+      marginBottom: 10,
   }
 });
 export default HomeScreen
